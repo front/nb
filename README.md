fbe43d26cacf3444607ec3c2093f3331efcc4d24
Roberto Ornelas <rob@front.no>
2013-07-16 16:06:31 +0100

    #3437 Implement Smacss skeleton, and use compass gem of normalize lib
    - To install normalize, run $gem install compass-normalize

---
+++ README.md
@@ -0,0 +1,10 @@
+# News Front prototype
+
+## Required Ruby GEMs
+Toolkit
+[github.com/Team-Sass/toolkit](https://github.com/Team-Sass/toolkit)
+    $gem install toolkit
+
+Susy
+
+Breakpoint
--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 # Require any additional compass plugins here.
 require 'zurb-foundation'
 
+require 'toolkit'
+require 'compass-normalize'
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
